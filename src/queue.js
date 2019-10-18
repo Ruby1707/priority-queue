993f04c9a2d198f2bc7a5beb96827683db342cc0
Ruby1707 <53220616+Ruby1707@users.noreply.github.com>
2019-10-19 00:12:53 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,12 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(maxSize==null){
+			this.maxSize=30;
+		}
+		else{
+	    	this.maxSize=maxSize;
+    	}
 	}
 
 	push(data, priority) {
